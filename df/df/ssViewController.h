23ba3b90ba5feab20ce0119185c61b1e1d1ce8e0
lanou <1052340662@qq.com>
2015-07-02 10:57:02 +0800

    jfhfj

---
+++ df/df/ssViewController.h
@@ -0,0 +1,13 @@
+//
+//  ssViewController.h
+//  df
+//
+//  Created by lanou on 15/7/2.
+//  Copyright (c) 2015年 lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ssViewController : UIViewController
+
+@end
---
+++ df/df/ssViewController.m
@@ -0,0 +1,37 @@
+//
+//  ssViewController.m
+//  df
+//
+//  Created by lanou on 15/7/2.
+//  Copyright (c) 2015年 lanou. All rights reserved.
+//
+
+#import "ssViewController.h"
+
+@interface ssViewController ()
+
+@end
+
+@implementation ssViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
