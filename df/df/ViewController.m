b75c928c54df148af09d243c98ec6ce17321c9c7
lanou <1052340662@qq.com>
2015-07-02 09:49:48 +0800

    111

--- df/df/ViewController.m
+++ df/df/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"dfhksdhfsad");
+    NSLog(@"222222222");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
